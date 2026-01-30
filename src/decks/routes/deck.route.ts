1958a1cb212cfb452d83a911305ae088cdaaa0dc
Simon LS <simonleclercq62850@gmail.com>
2026-01-30 17:52:05 +0100

    feat: Ajout de la création et la suppresion du deck

---
+++ src/decks/routes/deck.route.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import * as deckController from '../controllers/deck.controller';
+import { authenticateToken } from '../../auth/middlewares/auth.middleware';
+
+export const deckRouter = Router();
+
+// All routes require a JWT
+deckRouter.use(authenticateToken);
+
+deckRouter.post('/', deckController.create);
+deckRouter.get('/mine', deckController.getMine);
+deckRouter.get('/:id', deckController.getOne);
+deckRouter.patch('/:id', deckController.update);
+deckRouter.delete('/:id', deckController.remove);
