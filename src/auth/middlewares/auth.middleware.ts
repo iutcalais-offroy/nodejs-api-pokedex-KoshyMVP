5479d8c6f2b2ac320a3030f23354bf87da316d98
Simon LS <simonleclercq62850@gmail.com>
2026-01-30 14:57:39 +0100

    feat: Ajout de route et de middleware pour la récupération du token

---
+++ src/auth/middlewares/auth.middleware.ts
@@ -0,0 +1,32 @@
+import { NextFunction, Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+
+export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
+    // Extract the token 
+    const authHeader = req.headers.authorization;
+    const token = authHeader && authHeader.split(' ')[1]; 
+
+    if (!token) {
+        // 401 if the token is absent
+        return res.status(401).json({ error: 'Missing token' }); 
+    }
+
+    try {
+        // Check and decode the token
+        const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as {
+            userId: number;
+            email: string;
+        };
+
+        // Data attached to the request
+        req.user = {
+            userId: decoded.userId,
+            email: decoded.email
+        };
+
+        next(); 
+    } catch (error) {
+        // 401 if the token is invalid
+        return res.status(401).json({ error: 'Invalid or expired token' });
+    }
+};
