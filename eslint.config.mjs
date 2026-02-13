092da37ef03939befb78a4d417104e5420a872da
Simon LS <simonleclercq62850@gmail.com>
2026-02-13 11:03:23 +0100

    feat(auth): Ajout et test des commit valide

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,11 +3,14 @@ import tseslint from 'typescript-eslint'
 import eslintPluginPrettier from 'eslint-plugin-prettier/recommended'
 
 export default tseslint.config(
+  {
+    ignores: ['public/', 'dist/', 'node_modules/*', '.config.js'],
+  },
+
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   eslintPluginPrettier,
   {
-    ignores: ["src/generated/**/*"],
     rules: {
       '@typescript-eslint/no-unused-vars': 'error',
       '@typescript-eslint/no-explicit-any': 'warn',
